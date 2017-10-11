7984de07419d46a6d3d360ffd457a8388b0a9141
Luciana P <lucianasl@gmail.com>
2017-10-10 22:31:55 -0300

    added servlet

--- src/main/java/SimpleServer.java
+++ src/main/java/SimpleServer.java
@@ -1,15 +1,21 @@
-import handlers.HelloWorldHandler;
+import handlers.HelloServlet;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+
 
 public class SimpleServer {
 
     public static void main( String[] args ) throws Exception
     {
         Server server = new Server(8080);
-        server.setHandler(new HelloWorldHandler());
+        ServletContextHandler handler = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        server.setHandler(handler);
+        handler.addServlet(HelloServlet.class, "/*");
         server.start();
         server.dumpStdErr();
         server.join();
     }
 
 }
+
+
